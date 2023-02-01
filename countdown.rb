b0318bc3d5fd3fefed2f911725b8a56ab6aa1cc4
Ebyan Jamal <Ebyanjamal6@gmail.com>
2023-02-01 14:01:51 -0700

    commit tree 5f66e8d10f97407e9c6abcb2e7864c9c01db9d56

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+
+# integer = 10
+
+while integer < 10 
+    puts 
+    "#{integer} SECOND(S)!"
+integer -= 1 
+end
+puts "HAPPY NEW YEAR!"
 end
